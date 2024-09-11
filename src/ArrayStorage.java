1d563c4dd8039441f131f39d7ec1c570ae147c10
mchudin <mchudin@test.ru>
2024-09-11 11:31:34 +0500

    Initial fixes

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,48 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+	Resume[] storage = new Resume[10000];
+	private int size = 0;
 
-    void clear() {
-    }
+	void clear() {
+	}
+	
+	void update(Resume r) {
+	}
+	
+	void save(Resume r) {
+		if (uuuid == storage[i].getUuid()) {
+			return storage[i];
+		}
+	}
 
-    void save(Resume r) {
-    }
+	Resume get(String uuid) {
+		for (int i = 0; i < size; i++) {
+			if (uuuid == storage[i].getUuid()) {
+				return storage[i];
+			}
+		}
+		return null;
+	}
 
-    Resume get(String uuid) {
-        return null;
-    }
+	void delete(String uuid) {
+		for (int i = 0; i < size; i++) {
+			if (uuuid == storage[i].getUuid()) {
+				storage[i] = storage[size - 1];
+				storage[i] = null;
+				size--;
+			}
+		}
+	}
 
-    void delete(String uuid) {
-    }
+	/**
+	 * @return array, contains only Resumes in storage (without null)
+	 */
+	Resume[] getAll() {
+		return new Resume[0];
+	}
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    int size() {
-        return 0;
-    }
+	int size() {
+		return size;
+	}
 }
