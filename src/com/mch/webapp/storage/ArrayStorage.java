e1c0493cec3be3cfcddd311f7d8481e769de3fcc
mchudin <mchudin@test.ru>
2024-09-30 11:55:28 +0500

    FIx equals

--- src/com/mch/webapp/storage/ArrayStorage.java
+++ src/com/mch/webapp/storage/ArrayStorage.java
@@ -65,7 +65,7 @@ public class ArrayStorage extends AbstractArrayStorage {
 	@Override
 	protected int getIndex(String uuid) {
 		for (int i = 0; i < size; i++) {
-			if (uuid == storage[i].getUuid()) {
+			if (uuid.equals(storage[i].getUuid())) {
 				return i;
 			}
 		}
